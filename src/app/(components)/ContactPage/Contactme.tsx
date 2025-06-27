00c1c78fb6f6b604bc9728321b244be186b0808f
Sho8000 <yoshikichi8000@gmail.com>
2025-06-27 02:29:42 -0700

    created form POST for PC

--- src/app/(components)/ContactPage/Contactme.tsx
+++ src/app/(components)/ContactPage/Contactme.tsx
@@ -5,14 +5,47 @@ import NormalBtn from "../Btn/NormalBtn";
 
 import { useLangStore } from "@/store/useLangStore";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Contactme() {
   const {data} = useLangStore();
+  const [formData, setformData] = useState({
+    firstName: "",
+    lastName: "",
+    email:"",
+    message:"",
+  });
 
-  const handleSubmit = async (e: React.FormEvent) => {
-  e.preventDefault();
+  const formDataSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    
+    /*if no data then quit submitting */
+    if(!formData.firstName || !formData.lastName || !formData.email || !formData.message){
+      alert("Please Input Information")
+      return null;
+    }
 
-};
+    const response = await fetch("/api/sendEmail", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ firstName:formData.firstName, lastName:formData.lastName, email:formData.email, message:formData.message }),
+    });
+
+    if(response.ok) {
+      setformData({
+        firstName:"",
+        lastName:"",
+        email:"",
+        message:"",
+      })
+    } else {
+      alert("couldn't send your message,,,")
+    }
+    return null;
+  }
+
   return (
     <section className="w-[100%] bg-[#f2f2ff] pb-[5rem]">
       <div className="text-center mb-[3rem]">
@@ -29,13 +62,41 @@ export default function Contactme() {
             className={`w-[80%] h-auto aspect-square object-cover rounded-[50%] border-[4px] border-white`}
           />
         </div>
-        <form className="w-[40%] flex flex-col justify-center gap-[2rem]" onSubmit={handleSubmit}>
+        <form className="w-[40%] flex flex-col justify-center gap-[2rem]" onSubmit={formDataSubmit}>
           <div className="flex justify-between">
-            <input className={`w-[45%] ${Styles.inputBox} ${Styles.placeholder}`} type="text" placeholder="First Name" required/>
-            <input className={`w-[45%] ${Styles.inputBox} ${Styles.placeholder}`} type="text" placeholder="Last Name"/>
+            <input 
+              className={`w-[45%] ${Styles.inputBox} ${Styles.placeholder}`} 
+              type="text" 
+              placeholder="First Name"
+              value={formData.firstName}
+              onChange={(e)=> setformData({ ...formData,firstName:e.target.value})}
+              required
+            />
+            <input 
+              className={`w-[45%] ${Styles.inputBox} ${Styles.placeholder}`}
+              type="text" 
+              placeholder="Last Name"
+              value={formData.lastName}
+              onChange={(e)=> setformData({ ...formData,lastName:e.target.value})}
+              required
+            />
           </div>
-          <input className={`${Styles.inputBox} ${Styles.placeholder}`} type="email" placeholder="email"/>
-          <textarea className={`${Styles.messageBox} ${Styles.placeholder} pt-[0.5rem]`} name="" rows={5} id="" placeholder="message,,,"/>
+          <input
+            className={`${Styles.inputBox} ${Styles.placeholder}`}
+            type="email"
+            placeholder="email"
+            value={formData.email}
+            onChange={(e)=> setformData({ ...formData,email:e.target.value})}
+            required
+          />
+          <textarea
+            className={`${Styles.messageBox} ${Styles.placeholder} pt-[0.5rem]`}
+            rows={5}
+            placeholder="message,,,"
+            value={formData.message}
+            onChange={(e)=> setformData({ ...formData,message:e.target.value})}
+            required
+          />
           <NormalBtn text="detail"/>
         </form>
       </div>
--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -1,6 +1,5 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
-
 
 export async function GET(){
   const transporter = nodemailer.createTransport({
@@ -23,6 +22,50 @@ export async function GET(){
       text: "message is here from yoshikichi8000",
     });
     return NextResponse.json({ "send":"email"});
+
+  } catch(error){
+    console.error("Error sending email:", error);
+    return NextResponse.json({ success: false, error: "Failed to send email" });
+  }
+}
+
+export async function POST(request: NextRequest){
+  const body = await request.json();
+  const { firstName, lastName, email, message } = body;
+
+  if (!firstName || !lastName || !email || !message) {
+    return NextResponse.json({ message: "Missing information" });
+  }
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_APP_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  try{
+    await transporter.sendMail({
+      from: `${firstName} ${lastName} <${process.env.GMAIL_USER}>`,
+      to: process.env.MY_EMAIL,
+      subject: `New Message from my portfolio`,
+      html: `
+        <h2>New Contact Message</h2>
+        <p><strong>Name:</strong> ${firstName} ${lastName}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Message:</strong></p>
+        <p>${message.replace(/\n/g, "<br>")}</p>
+        `
+      });
+
+    return NextResponse.json({
+      status: "success",
+      message: "send message successfully",
+    });
 
   } catch(error){
     console.error("Error sending email:", error);
