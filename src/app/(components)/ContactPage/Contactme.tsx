a7990e7f6c70f283185cf1f431202e4996beb24b
Sho8000 <yoshikichi8000@gmail.com>
2025-06-27 11:20:24 -0700

    fixed responsive of contact page

--- src/app/(components)/ContactPage/Contactme.tsx
+++ src/app/(components)/ContactPage/Contactme.tsx
@@ -48,58 +48,60 @@ export default function Contactme() {
   }
 
   return (
-    <section className="w-[100%] bg-[#f2f2ff] pb-[5rem]">
-      <div className="text-center mb-[3rem]">
-        <h2 className={`${Styles.mainFont} ${Styles.titleArea}`}>Let&apos;s Start Conversation</h2>
-        <p className={`${Styles.subFont}`}>Ask how I can help you</p>
-      </div>
-      <div className={`w-[90%] flex justify-center gap-[3rem] m-auto`}>
-        <div className={`w-[40%] flex justify-center items-center`}>
-          <Image
-            src={data.landing.myPic}
-            alt="Main Image"
-            width={1200}
-            height={1600}
-            className={`w-[80%] h-auto aspect-square object-cover rounded-[50%] border-[4px] border-white`}
-          />
+    <section className={`w-[100%] min-h-[70vh] flex items-center justify-center bg-[#f2f2ff] ${Styles.sectionResponsive}`}>
+      <div>
+        <div className={`text-center ${Styles.titleContainer}`}>
+          <h2 className={`${Styles.mainFont} ${Styles.titleArea}`}>Let&apos;s Start Conversation</h2>
+          <p className={`${Styles.subFont}`}>Ask how I can help you</p>
         </div>
-        <form className="w-[40%] flex flex-col justify-center gap-[2rem]" onSubmit={formDataSubmit}>
-          <div className="flex justify-between">
-            <input 
-              className={`w-[45%] ${Styles.inputBox} ${Styles.placeholder}`} 
-              type="text" 
-              placeholder="First Name"
-              value={formData.firstName}
-              onChange={(e)=> setformData({ ...formData,firstName:e.target.value})}
-              required
-            />
-            <input 
-              className={`w-[45%] ${Styles.inputBox} ${Styles.placeholder}`}
-              type="text" 
-              placeholder="Last Name"
-              value={formData.lastName}
-              onChange={(e)=> setformData({ ...formData,lastName:e.target.value})}
-              required
+        <div className={`w-[90%] flex justify-center gap-x-[3rem] gap-y-[1rem] m-auto ${Styles.formContainer}`}>
+          <div className={`flex justify-center items-center ${Styles.imageArea}`}>
+            <Image
+              src={data.landing.myPic}
+              alt="Main Image"
+              width={1200}
+              height={1600}
+              className={`w-[80%] h-auto aspect-square object-cover rounded-[50%] border-[4px] border-white`}
             />
           </div>
-          <input
-            className={`${Styles.inputBox} ${Styles.placeholder}`}
-            type="email"
-            placeholder="email"
-            value={formData.email}
-            onChange={(e)=> setformData({ ...formData,email:e.target.value})}
-            required
-          />
-          <textarea
-            className={`${Styles.messageBox} ${Styles.placeholder} pt-[0.5rem]`}
-            rows={5}
-            placeholder="message,,,"
-            value={formData.message}
-            onChange={(e)=> setformData({ ...formData,message:e.target.value})}
-            required
-          />
-          <NormalBtn text="detail"/>
-        </form>
+          <form className={`flex flex-col justify-center ${Styles.formArea}`} onSubmit={formDataSubmit}>
+            <div className="flex justify-between">
+              <input 
+                className={`w-[45%] ${Styles.inputBox} ${Styles.placeholder}`} 
+                type="text" 
+                placeholder="First Name"
+                value={formData.firstName}
+                onChange={(e)=> setformData({ ...formData,firstName:e.target.value})}
+                required
+              />
+              <input 
+                className={`w-[45%] ${Styles.inputBox} ${Styles.placeholder}`}
+                type="text" 
+                placeholder="Last Name"
+                value={formData.lastName}
+                onChange={(e)=> setformData({ ...formData,lastName:e.target.value})}
+                required
+              />
+            </div>
+            <input
+              className={`${Styles.inputBox} ${Styles.placeholder}`}
+              type="email"
+              placeholder="email"
+              value={formData.email}
+              onChange={(e)=> setformData({ ...formData,email:e.target.value})}
+              required
+            />
+            <textarea
+              className={`${Styles.messageBox} ${Styles.placeholder} pt-[0.5rem]`}
+              rows={5}
+              placeholder="message,,,"
+              value={formData.message}
+              onChange={(e)=> setformData({ ...formData,message:e.target.value})}
+              required
+            />
+            <NormalBtn text="detail"/>
+          </form>
+        </div>
       </div>
     </section>
   );
